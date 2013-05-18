c184bb8437720d76b75e1e5076c8a8dbae1bac6b
Kevin Pluck <kevin.pluck@ivendi.com>
2013-05-18 01:33:13 +0100

    Initial commit

---
+++ KevsMath/MovingAverage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KevsMath
+{
+    public class MovingAverage
+    {
+        private int subSetSize = 1;
+        private List<double> list;
+
+        public MovingAverage()
+        {
+            list = new List<double>();
+        }
+
+        public MovingAverage(int subSetSize)
+            : this()
+        {
+            this.subSetSize = subSetSize;
+        }
+
+        public void add(double item)
+        {
+            list.Add(item);
+            if (list.Count > subSetSize) list.RemoveRange(0, 1);
+        }
+
+        public void setSubSetSize(int n)
+        {
+            subSetSize = n;
+        }
+
+        public double calculate()
+        {
+            if (list.Count == 0) return 0;
+
+            double sum = 0;
+
+            foreach (double item in list)
+            {
+                sum += item;
+            }
+
+            return sum / list.Count;
+        }
+    }
+}
---
+++ KevsMath/MovingAverageTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace KevsMath
+{
+    using NUnit.Framework;
+    [TestFixture]
+    class MovingAverageTest
+    {
+        MovingAverage movingAverage;
+        [SetUp]
+        public void init()
+        {
+            movingAverage = new MovingAverage();
+        }
+
+        [TestCase(3, new double[] { 1, 2, 3 }, Result = 2)]
+        [TestCase(3, new double[] { 2, 3, 4 }, Result = 3)]
+        [TestCase(3, new double[] { 1, 2, 3, 4 }, Result = 3)]
+        [TestCase(3, new double[] { 3, 4 }, Result = 3.5)]
+        public double testMovingAverage(int n, double[] list)
+        {
+            movingAverage.setSubSetSize(n);
+
+            addItemsToMovingAverage(list);
+
+            return movingAverage.calculate();
+        }
+
+
+        [TestCase(new double[] { 1, 2, 3, 4 }, Result = 3)]
+        public double testMovingAverageWithSubSetSizeSetInConstructor(double[] list)
+        {
+            movingAverage = new MovingAverage(3);
+            addItemsToMovingAverage(list);
+            return movingAverage.calculate();
+        }
+
+        [TestCase(3, new double[] { }, Result = 0)]
+        public double testMovingAverageWithEmptyList(int n, double[] list)
+        {
+            movingAverage.setSubSetSize(n);
+            addItemsToMovingAverage(list);
+            return movingAverage.calculate();
+        }
+
+        [TestCase(0, new double[] {1,2,3 }, Result = 0)]
+        public double testMovingAverageWith0SubSetSize(int n, double[] list)
+        {
+            movingAverage.setSubSetSize(n);
+            addItemsToMovingAverage(list);
+            return movingAverage.calculate();
+        }
+
+        private void addItemsToMovingAverage(double[] list)
+        {
+            foreach (var item in list)
+            {
+                movingAverage.add(item);
+            }
+        }
+    }
+}
